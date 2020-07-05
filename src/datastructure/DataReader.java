647032b9397a91b2f692a53df7b9f439ec9a271c
ZahidurRahmanJoy <brahmans.zoy@gmail.com>
2020-07-05 14:49:49 -0400

    Done so far: math.problems: 4 , DataStructure 2 and ansered some ques from defination

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,27 +1,34 @@
 package datastructure;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class DataReader {
 
 	public static void main(String[] args) {
-		/*
-		 * User API to read the below textFile and print to console.
-		 * Use BufferedReader class. 
-		 * Use try....catch block to handle Exception.
-		 *
-		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
-		 *
-		 * After reading from file using BufferedReader API, store each word into Stack and LinkedList. So each word
-		 * should construct a node in LinkedList.Then iterate/traverse through the list to retrieve as FIFO
-		 * order from LinkedList and retrieve as FILO order from Stack.
-		 *
-		 * Demonstrate how to use Stack that includes push,peek,search,pop elements.
-		 * Use For Each loop/while loop/Iterator to retrieve data.
-		 */
+		BufferedReader objReader = null;
+		try {
+			String strCurrentLine;
 
-		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+			objReader = new BufferedReader(String.valueOf(new FileReader("user.dir")) + "/src/data/self-driving-car.txt";
 
+			while ((strCurrentLine = objReader.readLine()) != null) {
 
+				System.out.println(strCurrentLine);
+			}
 
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+				if (objReader != null)
+					objReader.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
-
 }
